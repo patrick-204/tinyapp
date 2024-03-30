efc49b954570c41bc41cef81eb24a6cc42adc523
patrick-204 <p.ohara921@gmail.com>
2024-03-30 13:19:48 -0400

    Create HTTP server using express framework.

---
+++ express_server.js
@@ -0,0 +1,36 @@
+// Import the express library
+const express = require('express');
+
+// Define the app as an instance of express
+const app = express();
+
+// Define default port for server to listen on
+const PORT = 8080;
+
+const urlDatabase = {
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+};
+
+// Calls the callback function that has response "Hello" when GET request is made to 
+// root path of the server
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+// Calls the callback function that has sends a JSON response to the client when 
+// a GET request is sent to the /urls.json path of the server
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+// Call the callback function that sends HTML code to client browser (not actaully, but could)
+// when a GET request is sent to the /Hello path of the server
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+// Initialize server to listen on PORT for incoming HTTP requests
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+});
